5cd730b12ad4dc2d45877a0f71a1a8851f622d02
root <root@DESKTOP-MOTADELI>
2024-07-01 21:38:06 -0400

    delete module cloud run

--- main.tf
+++ main.tf
@@ -28,29 +28,3 @@ module "registry" {
   repository_name = var.repository_name
   region_name     = var.region_name
 }
-
-module "production_application" {
-  source       = "./modules/cloud-run"
-  service_name = "production-application-alessadelisio"
-  project_id   = var.project_id
-  region_name  = var.region_name
-  image        = "gcr.io/${var.project_id}/production-application-alessadelisio"
-}
-
-module "staging_application" {
-  source       = "./modules/cloud-run"
-  service_name = "staging-application-alessadelisio"
-  project_id   = var.project_id
-  region_name  = var.region_name
-  image        = "gcr.io/${var.project_id}/staging-application-alessadelisio"
-}
-
-# import {
-#   id = "locations/${var.region_name}/namespaces/${var.project_id}/services/staging-application-alessadelisio"
-#   to = module.staging_application.google_cloud_run_service.service
-# }
-
-# import {
-#   id = "locations/${var.region_name}/namespaces/${var.project_id}/services/production-application-alessadelisio"
-#   to = module.production_application.google_cloud_run_service.service
-# }
