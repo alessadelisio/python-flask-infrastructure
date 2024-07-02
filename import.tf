0a233ea26a9d208a0d03daabb46053999374e32f
root <root@DESKTOP-MOTADELI>
2024-07-01 21:06:22 -0400

    import resource of cloud run staging and production

--- import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  id = "locations/${var.region_name}/namespaces/${var.project_id}/services/staging-application-alessadelisio"
-  to = module.staging_application.google_cloud_run_service.service
-}
-
-import {
-  id = "locations/${var.region_name}/namespaces/${var.project_id}/services/production-application-alessadelisio"
-  to = module.production_application.google_cloud_run_service.service
-}
--- main.tf
+++ main.tf
@@ -44,3 +44,13 @@ module "staging_application" {
   region_name  = var.region_name
   image        = "gcr.io/${var.project_id}/staging-application-alessadelisio"
 }
+
+# import {
+#   id = "locations/${var.region_name}/namespaces/${var.project_id}/services/staging-application-alessadelisio"
+#   to = module.staging_application.google_cloud_run_service.service
+# }
+
+# import {
+#   id = "locations/${var.region_name}/namespaces/${var.project_id}/services/production-application-alessadelisio"
+#   to = module.production_application.google_cloud_run_service.service
+# }
